f158619d1516be9acd625316f9e8fab1077e46a3
Seongwoo Hong <silton0506@gmail.com>
2024-10-25 16:15:50 +0900

    World menu added

--- src/Bulletin.jsx
+++ src/Bulletin.jsx
@@ -1,13 +1,12 @@
-// The option to chat in world
-// The option to add character to world
-// The world description
-// will be available here
+// Character information on right chart
+// Option to edit character
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Dropdown from './Dropdown.jsx'
 // import DropdownGuest from './DropdownGuest.jsx'
 import DropdownOwner from './DropdownOwner.jsx'
+import BulletinEdit from "./BulletinEdit.jsx"
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
@@ -19,16 +18,7 @@ createRoot(document.getElementById('root')).render(
     <div className="container-fluid">
       <div className="row flex-nowrap">
         <DropdownOwner />
-        <div style={{marginTop:"76px"}}>
-          <img src='placeholder.png'></img>
-          <h1>World Title</h1>
-          <p>World Story</p>
-          <a className="btn btn-primary" href="#" role="button">Enter World Chat</a> <br></br>
-          <br></br>
-          <a class="btn btn-secondary" href="#" role="button">Add Character to World</a> 
-        </div>
-        <h1>Description</h1>
-        <p>Dummy text</p>
+        <BulletinEdit />
       </div>
     </div>
   </StrictMode>,
