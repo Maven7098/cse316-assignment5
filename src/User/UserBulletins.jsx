8361e50479a9f40fb310951251aa30065df393aa
Seongwoo Hong <silton0506@gmail.com>
2024-11-16 16:21:55 +0900

    Comments fixed and now correctly points out which modal is which

--- src/User/UserBulletins.jsx
+++ src/User/UserBulletins.jsx
@@ -75,68 +75,69 @@ const UserBulletins = (currentUser, selectedUser) => {
               {/* TODO: Add a modal form to write a new message */}
               <button type="button" className="grid-member card btn btn-primary" data-bs-toggle="modal" data-bs-target={`#writeModal`} style={{width: "18rem", textAlign:"center"}}>Write!</button>
 
+              {/* A modal to write */}
               <div className="card-modal">
-                      <div className="modal fade" id={`writeModal`} tabIndex="-1" aria-labelledby="postModalLabel" aria-hidden="true">
-                        <div className="modal-dialog">
-                          <div className="modal-content">
-                            <div className="modal-header">
-                              <h1 className="modal-title fs-5" id="postModalLabel">New Message</h1>
-                              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div className="modal-body">
-                            <form onSubmit={addNewPost} className='realForm'>
-                                {/* Date to be used */}
-                                <label htmlFor="postTitle" className="form-label">Title:</label>
-                                <br></br>
-                                <input type="text" name="postTitle" value={newPost.postTitle} onChange={onChangeForm} className="form-control"></input>
-                                <br></br>
-                                {/* Your story */}
-                                <label htmlFor="postContent">Story: </label>
-                                <br></br>
-                                {/* The textarea for React is slightly different from normal HTML */}
-                                <textarea value={newPost.postContent} name='postContent' onChange={onChangeForm} className="form-control"></textarea>
-                                <br></br>
-                                <button type="submit" className="btn btn-primary">Submit</button>
-                            </form>
-                            </div>
-                            <div className="modal-footer">
-                              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                            </div>
-                          </div>
-                        </div>
+                <div className="modal fade" id={`writeModal`} tabIndex="-1" aria-labelledby="postModalLabel" aria-hidden="true">
+                  <div className="modal-dialog">
+                    <div className="modal-content">
+                      <div className="modal-header">
+                        <h1 className="modal-title fs-5" id="postModalLabel">New Message</h1>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div className="modal-body">
+                      <form onSubmit={addNewPost} className='realForm'>
+                        {/* Post title */}
+                        <label htmlFor="postTitle" className="form-label">Title:</label>
+                        <br></br>
+                        <input type="text" name="postTitle" value={newPost.postTitle} onChange={onChangeForm} className="form-control"></input>
+                        <br></br>
+                        {/* Your story */}
+                        <label htmlFor="postContent">Story: </label>
+                        <br></br>
+                        {/* The textarea for React is slightly different from normal HTML */}
+                        <textarea value={newPost.postContent} name='postContent' onChange={onChangeForm} className="form-control"></textarea>
+                        <br></br>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                      </form>
+                      </div>
+                      <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
                       </div>
                     </div>
+                  </div>
+                </div>
+              </div>
 
             {assets.map((post) => (
-                // This consists of a character frame
-                <div className="grid-member card" style={{width: "18rem"}}>
-                    <div className="card-body">
-                        <h5 className="card-title">{post.postTitle}</h5>
-                        <p className="card-text">{post.postContent}</p>
-                        {/* Upon clicking this button, a modal will pop up */}
-                        <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target={`#postModal${post.postId}`}>More...</button>
-                    </div>
+              // This consists of a character frame
+              <div className="grid-member card" style={{width: "18rem"}}>
+                <div className="card-body">
+                  <h5 className="card-title">{post.postTitle}</h5>
+                  <p className="card-text">{post.postContent}</p>
+                  {/* Upon clicking this button, a modal will pop up */}
+                  <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target={`#postModal${post.postId}`}>More...</button>
+                </div>
 
-                    {/* A modal... to write? */}
-                    <div className="card-modal">
-                      <div className="modal fade" id={`postModal${post.postId}`} tabIndex="-1" aria-labelledby="postModalLabel" aria-hidden="true">
-                        <div className="modal-dialog">
-                          <div className="modal-content">
-                            <div className="modal-header">
-                              <h1 className="modal-title fs-5" id="postModalLabel">{post.postTitle}</h1>
-                              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div className="modal-body">
-                              <p>{post.postContent}</p>
-                            </div>
-                            <div className="modal-footer">
-                              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            </div>
-                          </div>
+                {/* A modal to read */}
+                <div className="card-modal">
+                  <div className="modal fade" id={`postModal${post.postId}`} tabIndex="-1" aria-labelledby="postModalLabel" aria-hidden="true">
+                    <div className="modal-dialog">
+                      <div className="modal-content">
+                        <div className="modal-header">
+                          <h1 className="modal-title fs-5" id="postModalLabel">{post.postTitle}</h1>
+                          <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                          <p>{post.postContent}</p>
+                        </div>
+                        <div className="modal-footer">
+                          <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                         </div>
                       </div>
                     </div>
                   </div>
+                </div>
+              </div>
             ))}
           </div>
         </div>
