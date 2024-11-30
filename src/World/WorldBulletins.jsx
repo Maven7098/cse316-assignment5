1529359c8de784e0fd3d5b64a605a052876c1bbc
Seongwoo Hong <silton0506@gmail.com>
2024-12-01 02:06:11 +0900

    Finally implemented the Bulletins, complete with reply and character selection functionality

--- src/World/WorldBulletins.jsx
+++ src/World/WorldBulletins.jsx
@@ -14,41 +14,22 @@ const WorldBulletins = ({currentUserId}) => {
     
     // Get a list of characters from this world.
     // See if any character's characterCreator matches the currentUser
-    const [characterList, setCharacterList] = useState([]);
-    
+    const [worldCharacters, setWorldCharacters] = useState([]);
     // This is how we grab a list of characters in this world.
     useEffect(() => {
       // Re-initialization for repeat calls
-      setCharacterList([]);
-      axios.get(`http://localhost:3000/api/worlds/${selectedWorld}`)
-        .then(function (response){
-          console.log(JSON.parse(response.data.worldCharacters))
-          // Parse the worldCharacters back into array, and map them to get the actual characters
-          JSON.parse(response.data.worldCharacters).map((character)=>{axios.get(`http://localhost:3000/api/characters/${character}`)
-          .then(function (response){
-            setCharacterList(characterList => [...characterList, response.data])
-          })
-        })})
-        .catch(function (error) {
-          console.log(error);
-      })
+      setWorldCharacters([]);
+      axios.get(`http://localhost:3000/api/worlds/characters/${selectedWorld}`)
+        .then(res => setWorldCharacters(res.data))
+        .catch(err => console.log(err))
     }, []);
 
-    // Build the user object out of the userId provided
-    const [currentUserCharacters, setCurrentUserCharacters] = React.useState([]);
-    // Grab the characer list of the user (we need nothing else), only grabbing the IDs
-    React.useEffect(() => {
-        axios.get(`http://localhost:3000/api/users/${currentUserId}`)
-          .then(response => setCurrentUserCharacters(JSON.parse(response.data.userCharacters)))
-          .catch(error => console.log(error))
-        }, []);
-    console.log(characterList);
-    console.log(currentUserCharacters);
+    console.log(worldCharacters);
     
     // Does currentUser has a character in this world?
-    // This returns an intersection of the characterList and currentUserCharacters
+    // This returns an intersection of the worldCharacters and currentUserCharacters
     // which will be used in the modal
-    const currentUserHasCharacter = characterList.filter(character => currentUserCharacters.includes(character.characterId));
+    const currentUserHasCharacter = worldCharacters.filter(character => character.characterCreator == currentUserId);
     console.log(currentUserHasCharacter)
 
     // TODO: Add a backend to send the messages over
@@ -62,10 +43,10 @@ const WorldBulletins = ({currentUserId}) => {
 
     // Only for the select character part of the modal.
     const onChangeHandler = (event) => {
-      setFacilityData(places[event.target.value-1]);
+      setNewMessage(values => ({...values, messageSenderId: event.target.value}));
       console.log(
           "User Selected Value - ",
-          places[event.target.value-1]
+          event.target.value
       );
     };
 
@@ -106,7 +87,56 @@ const WorldBulletins = ({currentUserId}) => {
       <div className="container-fluid">
       <div className="row flex-nowrap">
             <div className="grid-container" style={{display:"flex", flexWrap:"wrap", marginTop:"72px", flex:"1"}}>
-              
+              {/* TODO: Add a modal form to write a new message */}
+              {/* Only if the current user has a character in this world */}
+              {/* If Write! button is used to write a message, set the replyId to 0 (no reply) */}
+              {currentUserHasCharacter &&
+                <button type="button" onClick={() => setNewMessage(values => ({...values, messageReplyId: 0}))} className="grid-member card btn btn-primary" data-bs-toggle="modal" data-bs-target={`#writeModal`} style={{width: "18rem", textAlign:"center"}}>Write!</button>
+              }
+              {/* A modal to write */}
+              <div className="card-modal">
+                <div className="modal fade" id={`writeModal`} tabIndex="-1" aria-labelledby="messageModalLabel" aria-hidden="true">
+                  <div className="modal-dialog">
+                    <div className="modal-content">
+                      <div className="modal-header">
+                        <h1 className="modal-title fs-5" id="messageModalLabel">New Message</h1>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div className="modal-body">
+                      <form onSubmit={addNewPost} className='realForm'>
+                        
+                        {/* Post title */}
+                        <label htmlFor="messageTitle" className="form-label">Title:</label>
+                        <br></br>
+                        <input type="text" name="messageTitle" value={newMessage.messageTitle} onChange={(event)=>onChangeForm(event,setNewMessage)} className="form-control"></input>
+                        
+                        {/* Select Character */}
+                        <label htmlFor="messageSenderId" className="form-label">Write as:</label>
+                        <select onChange={onChangeHandler} className="form-select" name="messageSenderId" id="messageSenderId">
+                        <br></br>
+                        {currentUserHasCharacter.map((character)=>{
+                            return (<option value={character.characterId}>{character.characterName}</option>)})}</select>
+                        <br></br>
+                        
+                        {/* Your message */}
+                        <label htmlFor="messageContent">Message: </label>
+                        <br></br>
+                        
+                        {/* The textarea for React is slightly different from normal HTML */}
+                        <textarea value={newMessage.messageContent} name='messageContent' onChange={(event)=>onChangeForm(event,setNewMessage)} className="form-control" maxLength={512}></textarea>
+                        <br></br>
+                        
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                      </form>
+                      </div>
+                      <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
               {/* Post table mapping should only be done if messageTable has more than 1 entry*/}
               {/* else "messageTable is not a function" error is thrown */}
               {messageTable.length > 0 &&
@@ -123,7 +153,7 @@ const WorldBulletins = ({currentUserId}) => {
                       
                       {/* This is to reply to this message */}
                       {/* Set the reply message to the replyId of this message */}
-                      <button type="button" onClick={setNewMessage(values => ({...values, messageReplyId: message.messageId}))} className="btn btn-primary" data-bs-toggle="modal" data-bs-target={`#writeModal`} >Reply...</button>
+                      <button type="button" className="btn btn-primary" onClick={() => setNewMessage(values => ({...values, messageReplyId: message.messageId}))} data-bs-toggle="modal" data-bs-target={`#writeModal`} >Reply...</button>
                     </div>
     
                     {/* A modal to read */}
@@ -137,6 +167,9 @@ const WorldBulletins = ({currentUserId}) => {
                             </div>
                             <div className="modal-body">
                               <p>{message.messageContent}</p>
+                              {message.messageReplyId != 0 &&(
+                                <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target={`#messageModal${message.messageReplyId}`}>More...</button>
+                              )}
                             </div>
                             <div className="modal-footer">
                               <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
